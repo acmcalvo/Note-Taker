f2fcd42f43be15f0b40c8df7ec44a6372cacd500
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-12-31 13:17:57 -0500

    WIP

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var express = require("express");
 // Tells node that we are creating an "express" server
 var app = express();
 
-// Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 3000;
+
+var PORT = process.env.PORT || 30001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
