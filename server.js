40c5c494cd739e835a64591a1004bebbcce9ed48
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-12-31 13:22:40 -0500

    WIP

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
-
+// Dependencies
 const express = require("express");
-
-// Tells node that we are creating an "express" server
 const app = express();
 
+// Set a  Port
 
 const PORT = process.env.PORT || 30001;
 
-// Sets up the Express app to handle data parsing
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-// The below code effectively "starts" our server
 app.listen(PORT, function () {
-	console.log("App listening on PORT: " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
