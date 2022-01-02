f953762a41f8409c23e066753b2723694374e596
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-02 16:54:27 -0500

    WIP

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ const express = require("express");
 const app = express();
 
 // Set a  Port
-
-const PORT = process.env.PORT || 30001;
-
+const PORT = process.env.PORT || 30000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+//require("./routes/apiRoutes")(app);
+//require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
