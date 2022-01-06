883f89c7aad84ec0119523ffa36ad9ce2f283214
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-05 22:54:05 -0500

    WIP

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
-//require("./routes/apiRoutes")(app);
-//require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
