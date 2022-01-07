0b65d8063a76d8b0caab2ff08d36b7c352f924fd
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-07 18:41:08 -0500

    WIP

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,41 +1,28 @@
-const db = require("../db/db.json");
-const fs = require("fs");
+// Dependencies
+const router = require("express").Router();
+const store = require("./../db/store");
 
-// ROUTING
+// routes
 
-module.exports = (app) => {
-  // API GET Request
-  app.get('/api/notes', (req, res) => {
-    // Read the db.json file and return all saved notes as JSON
-    res.json(db);
-  });
+router.get("/notes", function (req, res) {
+    store
+        .getNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err))
+});
 
-  // API POST Request
-  app.post('/api/notes', (req, res) => {
-    // Receive a new note to save on the request body, add it to the db.json file
-    db.push(req.body);
-    // Add unique id to each note
-    db.forEach((obj, i) => {
-      obj.id = i + 1;
-    });
-    // Return the new note to the client
-    fs.writeFile('./db/db.json', JSON.stringify(db), function () {
-      res.json(db);
-    });
-  });
+router.post("/notes", function (req, res) {
+    store
+        .addNote(req.body)
+        .then((notes) => res.json(notes))
+        .catch(err => res.status(500).json(err))
+});
 
-  // // API DELETE Request
-  app.delete('/api/notes/:id', (req, res) => {
-    var id = req.params.id;
-    // Use splice to delete the selected note from the db array
-    db.splice(id - 1, 1);
-    // Reassign id for each note object
-    db.forEach((obj, i) => {
-      obj.id = i + 1;
-    });
-    // Return the remaining notes to the client
-    fs.writeFile("./db/db.json", JSON.stringify(db), function () {
-      res.json(db);
-    });
-  });
-};
+router.delete("/notes/:title", function (req, res) {
+    store
+        .deleteNotes(req.params.title)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err))
+});
+
+module.exports = router;
