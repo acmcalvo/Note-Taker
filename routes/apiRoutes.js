3d220c7a3dbbcf8070f275b61110e083f9ed56d1
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-07 21:44:00 -0500

    WIP

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,89 +1,78 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+// The following API routes should be created:
+// * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+// * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, 
+// and then return the new note to the client. 
+
+// const notes = [];
 const fs = require('fs');
+const path = require('path');
+// This package will be used to generate our unique ids: https://www.npmjs.com/package/nanoid#js
+// const { nanoid } = require('nanoid');
+const notes = require('../db/db.json');
+// const router = require('express').Router();
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+module.exports = (app) => {
 
-module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-  // Routes - one to index, one to notes
-  app.get('/api/notes', function(req, res) {
-    fs.readFile('./db/db.json', (err, data) => {
-      if (err) throw err;
-      dbData = JSON.parse(data);
-      res.send(dbData);
+    // retrieve stored notes from database
+    
+    app.get('/api/notes', (req, res) => res.json(notes));
+
+    // app.get('/api/notes', (req, res) => {
+    //     fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, jsonString => {
+    //         if (err) {
+    //             console.log(err)
+    //             return;
+    //         }
+    //         res.json(JSON.parse(jsonString));
+    //     }));
+    // });
+
+    // app.post('/api/notes', (req, res) => {
+    //     fs.readFile(path.join(__dirname, 'db.json'), 'utf8',
+    //         function (error, response) {
+    //             if (err) {
+    //                 console.log(error)
+    //                 return;
+    //             }
+    //             res.json(JSON.parse(jsonString));
+    //         })
+    // });
+
+   
+    
+    
+    // post new note to server
+    app.post('/api/notes', (req, res) => {    
+        let id = notes.length + 1
+        console.log(id);
+        req.body.id = parseInt(id);
+        // console.log(req.body)
+        notes.push(req.body);
+        fs.writeFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, jsonString => {
+            if (err) {
+                console.log(err)
+                return;
+            }
+            res.json(JSON.parse(jsonString));
+        }));
     });
-  });
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the array)
-  // ---------------------------------------------------------------------------
-
-  // API POST Requests
-
-  app.post('/api/notes', function(req, res) {
-    const userNotes = req.body;
-
-    fs.readFile('./db/db.json', (err, data) => {
-      if (err) throw err;
-      dbData = JSON.parse(data);
-      dbData.push(userNotes);
-      let number = 1;
-      dbData.forEach((note, index) => {
-        note.id = number;
-        number++;
-        return dbData;
-      });
-      console.log(dbData);
-
-      stringData = JSON.stringify(dbData);
-
-      fs.writeFile('./db/db.json', stringData, (err, data) => {
-        if (err) throw err;
-      });
+  
+    // const noteTitle = req.body;
+    // const noteText = req.body;
+    // const newNote = {
+    //     id: "noteId".id,
+    //     tite: 'noteTitle'.title,
+    //     text: 'noteText'.text,
+    // };
+    // note.push(newNote);
+    // res.json(newnote);
+      
+        app.delete('/api/notes/:id', (req, res) => {
+        let id = notes.length - 1
+        req.body.id = parseInt(id);
+        notes.pop(req.params.body);
+        res.json({ ok: true });
+        // res.json({ notes });
     });
-    res.send('Thank you for your note!');
-  });
 
-  // API DELETE Requests
-  // API route that allows user to delete a note and updates json data
-  app.delete('/api/notes/:id', function(req, res) {
-    // Gets id number of note to delete
-    const deleteNote = req.params.id;
-    console.log(deleteNote);
-
-    fs.readFile('./db/db.json', (err, data) => {
-      if (err) throw err;
-
-      // Comparing each note's id to delete note
-      dbData = JSON.parse(data);
-      // for each function, comparing each note's id to the chosen_for_death variable
-      for (let i = 0; i < dbData.length; i++) {
-        if (dbData[i].id === Number(deleteNote)) {
-          dbData.splice([i], 1);
-        }
-      }
-      console.log(dbData);
-      stringData = JSON.stringify(dbData);
-
-      fs.writeFile('./db/db.json', stringData, (err, data) => {
-        if (err) throw err;
-      });
-    });
-    // Express response.status(204)
-    res.status(204).send();
-  });
-};
+}; 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,28 +1,30 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
+// The following HTML routes should be created:
+// * `GET /notes` should return the `notes.html` file.
+// * `GET *` should return the `index.html` file.
+
+// const express = require('express'); 
 const path = require('path');
-// ===============================================================================
-// ROUTING
-// ===============================================================================
 
-module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
+module.exports = (router) => {
+ // HTML GET Requests
 
-  app.get('/notes', function(req, res) {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
 
-  app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
 
-  // If no matching route is found default to index
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
+router.get('/notes', (req ,res) => {
+     res.sendFile(path.join(__dirname, '../public/notes.html' ));
+
+    });
+
+router.get('*', (req ,res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html' ));
+   
+       });
+
+router.get('/index', (req ,res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html' ))
+   
+    });
+
 };
+
+// module.exports = router; 
