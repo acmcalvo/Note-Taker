e1c51afc5485b92847151398d832149ebd767c99
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-07 22:18:27 -0500

    WIP

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,7 @@
-// The following HTML routes should be created:
-// * `GET /notes` should return the `notes.html` file.
-// * `GET *` should return the `index.html` file.
 
-// const express = require('express'); 
 const path = require('path');
 
 module.exports = (router) => {
- // HTML GET Requests
-
-
 
 router.get('/notes', (req ,res) => {
      res.sendFile(path.join(__dirname, '../public/notes.html' ));
