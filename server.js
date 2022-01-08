bb3500c59d3d411ff28d5ece711d175d81abdf14
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-08 00:19:49 -0500

    wip

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
 //Dependencies
-const express = require('express');
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
 
-// set up express app for parsing 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Routes 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-// Notes Data
-
-
-// App listener to start server
 app.listen(PORT, () => {
-    console.log(`App is listening on PORT: ${PORT}`)
-
+  console.log(`App is listening on PORT: ${PORT}`);
 });
