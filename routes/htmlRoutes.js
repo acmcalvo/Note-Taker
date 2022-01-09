484d54d5409b9d6a6ea4491178cd0014374132a4
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-09 10:42:46 -0500

    find error line 4 "*' stay of "/" simple fix but take me time to find the error

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,11 @@
 const path = require("path");
 
 module.exports = (router) => {
-
-    router.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-      });
+  router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 
   router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
-
 };
