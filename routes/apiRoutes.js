a9f9f100d300890e9acb978a0170b30d545371a1
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-01-09 20:25:17 -0500

    fix error 500

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,53 @@
 const fs = require("fs");
-const path = require("path");
-
-const notes = require("../db/db.json");
 
 module.exports = (app) => {
-  app.get("/api/notes", (req, res) => res.json(notes));
+  let notes;
+  app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) throw err;
 
-  app.post("/api/notes", (req, res) => {
-    let id = notes.length + 1;
-    console.log(id);
-    req.body.id = parseInt(id);
+      res.json(JSON.parse(data));
+    });
+  });
 
-    notes.push(req.body);
-    fs.writeFile(
-      path.join(__dirname, "../db/db.json"),
-      "utf8",
-      (err,
-      (jsonString) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        res.json(JSON.parse(jsonString));
-      })
-    );
+  app.post('/api/notes', (req, res) => {
+    let newNote = req.body;
+
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+      let notes = JSON.parse(data);
+
+      notes.push(newNote);
+
+      notes.forEach((item, i) => {
+        item.id = 1 + i;
+      });
+
+      fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+        if (err) throw err;
+      });
+    });
+
+    res.json(newNote);
   });
 
   app.delete("/api/notes/:id", (req, res) => {
-    let id = notes.length - 1;
-    req.body.id = parseInt(id);
-    notes.pop(req.params.body);
-    res.json({ ok: true });
+    let delNoteId = req.params.id;
+
+    fs.readFile("./db/db.json", function (err, data) {
+      if (err) throw err;
+      let notes = JSON.parse(data);
+
+      notes.forEach((thisNote, i) => {
+        if (thisNote.id.toString() === delNoteId) {
+          notes.splice(i, 1);
+        }
+      });
+
+      fs.writeFile('./db/db.json', JSON.stringify(notes), function (err) {
+        if (err) throw err;
+      });
+    });
+    res.send('file');
   });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,11 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = (router) => {
-  router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+  router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
-  router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 };
